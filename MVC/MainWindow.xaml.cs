5422632e069ac8c11a791b47a1bf85529d4ed64d
Настя Жулина <julina.nastia2017@yandex.ru>
2023-02-05 18:44:22 +0300

    Добавьте файлы проекта.

---
+++ MVC/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            Models.tbOperation = Operation;
+            Models.ResultCalc = ResultCalc;
+
+            cbOperation.SelectedIndex = 0;
+
+        }
+
+        private void cbOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            Models.Combo = cbOperation.SelectedIndex;
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Models.Check(cbOperation.SelectedIndex, One.Text, Two.Text);
+        }
+    }
+}
---
+++ MVC/Models.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVC
+{
+    public static class Models
+    {
+        public static List<string> operationList = new List<string> { "+", "-", "*", "/" };
+               
+        public static TextBlock tbOperation;
+        public static TextBlock ResultCalc;
+     
+
+        public static int Combo
+        {
+            set
+            {
+                tbOperation.Text = operationList[value];
+            }
+        }
+        public static void Check(int index, string tbOne, string tbTwo)
+        {
+            try
+            {
+                double one = Convert.ToDouble(tbOne);
+                double two = Convert.ToDouble(tbTwo);
+
+                if (index == 3 && two == 0)
+                {
+                    ResultCalc.Text = "";
+                    MessageBox.Show("Ошибка деления на 0");
+                }
+                else
+                {
+                    ResultCalc.Text = Result(index, one, two);
+                }
+            }
+            catch
+            {
+                ResultCalc.Text = "";
+                MessageBox.Show("Введите данные корректно");
+            }
+        }
+        public static string Result(int index, double one, double two)
+        {
+            switch (index)
+            {
+                case 0:
+                    return (one + two).ToString();
+                case 1:
+                    return (one - two).ToString();
+                case 2:
+                    return (one * two).ToString();
+                case 3:
+                    return (one / two).ToString();
+                default:
+                    return "Ошибка";
+            }
+        }
+
+    }
+}
